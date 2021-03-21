a6f4075b5c25ed4662c86036ddc8c21ea9bf9798
Jay2020-malo <68229090+Jay2020-malo@users.noreply.github.com>
2021-03-22 00:36:58 +0400

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBI0JJuApKc_Vf-snRVAPuNacXjSaII8V0",
+    authDomain: "jamal-resume.firebaseapp.com",
+    projectId: "jamal-resume",
+    storageBucket: "jamal-resume.appspot.com",
+    messagingSenderId: "398438463202",
+    appId: "1:398438463202:web:f378414f273acfd10d50bb",
+    measurementId: "G-SGXJPM7DT2"
+  };
